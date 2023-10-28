23456d02720dc40df8830c60b8eaab2b3c94db12
Abdul Aziz <zizochampion@hotmail.com>
2023-10-28 13:06:11 +0200

    Delete Centipede.scala

--- Centipede.scala
+++
@@ -1,8 +0,0 @@
-class Centipede(bodySize : Int, bodyLength : Int = 50, follows : Follower) {
-  //will have legs
-  // will have behaviour
-  var segList : List[Segments] = List(Head(length = bodyLength))
-  for (i <- 0 until bodySize) addSeg()
-
-  private def addSeg(): Unit = segList = segList :+ Body(length = bodyLength)
-}
